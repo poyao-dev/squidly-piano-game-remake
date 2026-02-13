789c2ad3ad1f265a04bd192c60bc7e15b704faed
poyaohuang <poyaohg0705@gmail.com>
2026-02-14 01:47:06 +1100

    init project

---
+++ app.js
@@ -0,0 +1,71 @@
+const BASE_URL = new URL(".", import.meta.url).href;
+
+class SquidlyPianoGame {
+  constructor() {
+    this.audioElements = {
+      C: null,
+      D: null,
+      E: null,
+      F: null,
+      G: null,
+    };
+    this.init();
+  }
+
+  init() {
+    // Initialize the game here
+    console.log("Squidly Piano Game Initialized");
+    SquidlyAPI.firebaseSet("pianoKeyPressed", null); // Initialize the value in Firebase
+    this._setupAudioSources();
+    this._setupKeyboard();
+    this._setupListeners();
+  }
+
+  _setupAudioSources() {
+    // Preload audio sources for each key
+    for (const key in this.audioElements) {
+      const audio = new Audio(`${BASE_URL}sounds/${key}.mp3`);
+      this.audioElements[key] = audio;
+    }
+  }
+
+  _setupKeyboard() {
+    // 5 keys: C, D, E, F, G
+    // iterate over the keys on the keyboard and create buttons for them
+    for (const key in this.audioElements) {
+      const accessButtonWrapper = document.createElement("access-button");
+      const button = document.createElement("button");
+      button.textContent = key;
+      button.className =
+        "bg-white text-black font-bold py-4 px-6 rounded-lg shadow-lg m-2 transform transition hover:-translate-y-0.5 active:translate-y-0";
+
+      accessButtonWrapper.addEventListener("access-click", () => {
+        console.log(`Key ${key} pressed`);
+        // add a timestamp to the key press
+        SquidlyAPI.firebaseSet("pianoKeyPressed", key + "_" + Date.now());
+      });
+      accessButtonWrapper.appendChild(button);
+      document.body.appendChild(accessButtonWrapper);
+    }
+  }
+
+  _setupListeners() {
+    // Set up Firebase listeners or other event listeners here
+    SquidlyAPI.firebaseOnValue("pianoKeyPressed", (value) => {
+      // value will be in the format "key_timestamp", we only want the key
+      // null returned if the value is deleted, so we need to check for that
+      if (!value) return;
+      const key = value.split("_")[0];
+      console.log("Piano key pressed:", key);
+      // Reuse pre-loaded audio if available
+      if (this.audioElements[key]) {
+        this.audioElements[key].play();
+      }
+    });
+  }
+}
+
+// Instantiate the app to run it
+window.onload = () => {
+  new SquidlyPianoGame();
+};
